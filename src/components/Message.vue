dbf89baea381d056527d66eed07b844f97fe65f7
Jun Ukemori <jukemori@gmail.com>
2024-05-06 13:27:49 +0900

    add message component

---
+++ src/components/Message.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>{{ message }}</div>
+</template>
+
+<script>
+export default {
+  props: ["initialMessage"],
+  data() {
+    return {
+      message: this.initialMessage,
+    };
+  },
+  watch: {
+    initialMessage(newMessage) {
+      this.message = newMessage;
+    },
+  },
+};
+</script>
+
+<style scoped>
+/* Add styles as needed */
+</style>
