818146ec7f487650ed868646383f77e16df82f51
Jun Ukemori <jukemori@gmail.com>
2024-05-06 13:28:47 +0900

    add reversed message component

---
+++ src/components/ReversedMessage.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>{{ reversedMessage }}</div>
+</template>
+
+<script>
+export default {
+  props: ["reversedMessage"],
+};
+</script>
+
+<style scoped>
+/* Add styles as needed */
+</style>
