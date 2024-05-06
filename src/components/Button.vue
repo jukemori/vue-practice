22fa2aaf7801755be786dcedda18367df16c8534
Jun Ukemori <jukemori@gmail.com>
2024-05-06 13:28:18 +0900

    add button component

---
+++ src/components/Button.vue
@@ -0,0 +1,17 @@
+<template>
+  <button @click="handleClick">Reverse Message</button>
+</template>
+
+<script>
+export default {
+  methods: {
+    handleClick() {
+      this.$emit("button-clicked");
+    },
+  },
+};
+</script>
+
+<style scoped>
+/* Add styles as needed */
+</style>
